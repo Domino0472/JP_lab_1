9647c74a757ccd97d0bf8e67d5d25d51e7c7d92d
damia <damian.dominiak.2006@gmail.com>
2025-10-15 19:47:57 +0200

    adding 2 class Wektor2D.cs and Punkt2D.cs
    all completed

---
+++ JP_lab_1/Classes/Punkt2D.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace JP_lab_1.Classes;
+
+public class Punkt2D
+{
+    private double _x;
+    private double _y;
+
+    public double X => _x;
+    public double Y => _y;
+
+    public Punkt2D(double x, double y)
+    {
+       _x = x; 
+       _y = y;
+    }
+}
---
+++ JP_lab_1/Classes/Wektor2D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace JP_lab_1.Classes;
+
+public class Wektor2D
+{
+    private double _x;
+    private double _y;
+    
+    public double X => _x;
+    public double Y => _y;
+
+    public Wektor2D(double x, double y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public static Wektor2D CreatePointer(Punkt2D start, Punkt2D end)
+    {
+        return new Wektor2D(start.X-end.X, end.Y-start.Y);
+    }
+
+    public static Wektor2D Sum(Wektor2D w1, Wektor2D w2)
+    {
+        return new Wektor2D(w1.X + w2.X, w1.Y + w2.Y);
+    }
+
+    public static Wektor2D Odejmij(Wektor2D w1, Wektor2D w2)
+    {
+        return new Wektor2D(w1.X - w2.X, w1.Y - w2.Y);
+    }
+
+    public double Distance()
+    {
+        return Math.Sqrt(Math.Pow(_x, 2) + Math.Pow(_y, 2));
+    }
+    
+}
